db9e68365fe2227ca93be84b6a261b13f9a6b5d6
mariostefanov <mario.stefanov14@gmail.com>
2024-01-06 19:28:33 +0200

    Make validation on login form.

--- src/main/java/com/example/rentalcars/web/UserController.java
+++ src/main/java/com/example/rentalcars/web/UserController.java
@@ -7,8 +7,10 @@ import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/users")
@@ -20,14 +22,30 @@ public class UserController {
         this.userService = userService;
     }
 
+    @ModelAttribute("userModel")
+    public UserLoginDTO initUserModel() {
+        return new UserLoginDTO();
+    }
+
     @GetMapping("/login")
     public String login(){
         return "auth-login";
     }
 
     @PostMapping("/login")
-    public String login(UserLoginDTO userLoginDto){
-        userService.login(userLoginDto);
+    public String login(@Valid UserLoginDTO userLoginDTO,
+                        BindingResult bindingResult,
+                        RedirectAttributes redirectAttributes
+    ) {
+
+        if (bindingResult.hasErrors() || !this.userService.login(userLoginDTO)) {
+            redirectAttributes.addFlashAttribute("userModel", userLoginDTO);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.userModel",
+                    bindingResult);
+            bindingResult.rejectValue("password", "InvalidPasswordError", "Invalid password.");
+            return "redirect:/users/login";
+        }
+
         return "redirect:/";
     }
 
