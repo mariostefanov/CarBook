39444925323da1dd70c030fec05c1b7b3ea189c8
mariostefanov <mario.stefanov14@gmail.com>
2024-01-08 23:46:48 +0200

    Fix small bug.

--- src/main/java/com/example/rentalcars/web/UserController.java
+++ src/main/java/com/example/rentalcars/web/UserController.java
@@ -4,12 +4,10 @@ import com.example.rentalcars.model.dto.UserLoginDTO;
 import com.example.rentalcars.model.dto.UserRegisterDTO;
 import com.example.rentalcars.service.UserService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -18,6 +16,7 @@ public class UserController {
 
     private final UserService userService;
 
+    
     public UserController(UserService userService) {
         this.userService = userService;
     }
@@ -28,7 +27,7 @@ public class UserController {
     }
 
     @GetMapping("/login")
-    public String login(){
+    public String login() {
         return "auth-login";
     }
 
@@ -51,7 +50,7 @@ public class UserController {
 
 
     @GetMapping("/logout")
-    public String logout(){
+    public String logout() {
         userService.logout();
         return "redirect:/";
     }
