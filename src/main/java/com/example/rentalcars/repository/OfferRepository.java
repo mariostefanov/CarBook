89c5a3b6cb96f7d1a0a0244db16d56847955cebc
mariostefanov <mario.stefanov14@gmail.com>
2024-03-06 21:07:35 +0200

    Change id from Long to UUID

--- src/main/java/com/example/rentalcars/repository/OfferRepository.java
+++ src/main/java/com/example/rentalcars/repository/OfferRepository.java
@@ -4,6 +4,8 @@ import com.example.rentalcars.model.entity.OfferEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface OfferRepository extends JpaRepository<OfferEntity, Long> {
+public interface OfferRepository extends JpaRepository<OfferEntity, UUID> {
 }
