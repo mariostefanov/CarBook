d2ba4f9aea4174879281bb96ba9b17d2a08993ca
mariostefanov <mario.stefanov14@gmail.com>
2024-03-14 21:16:02 +0200

    Less Locations

--- src/main/java/com/example/rentalcars/model/enums/PuckUpAndDropOffLocation.java
+++ src/main/java/com/example/rentalcars/model/enums/PuckUpAndDropOffLocation.java
@@ -1,31 +1,9 @@
 package com.example.rentalcars.model.enums;
 
 public enum PuckUpAndDropOffLocation {
-    Blagoevgrad,
     Burgas,
-    Dobrich,
-    Gabrovo,
-    Haskovo,
-    Kardzhali,
-    Kyustendil,
-    Lovech,
-    Montana,
-    Pazardzhik,
-    Pernik,
-    Pleven,
     Plovdiv,
-    Razgrad,
-    Ruse,
-    Shumen,
-    Silistra,
-    Sliven,
-    Smolyan,
     Sofia ,
-    Stara_Zagora,
-    Targovishte,
     Varna,
-    Veliko_Tarnovo,
-    Vidin,
-    Vratsa,
-    Yambol
+
 }
